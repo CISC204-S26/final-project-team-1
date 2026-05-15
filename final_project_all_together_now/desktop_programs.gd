39a6ab80915657468d263b96b8423bacfc53520e
Katie-H258 <katie.hardy258@gmail.com>
2026-05-15 04:49:27 -0400

    added program bases, tried figuring out enemy debug log jargon

--- final_project_all_together_now/desktop_programs.gd
+++ final_project_all_together_now/desktop_programs.gd
@@ -9,3 +9,33 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_map_button_pressed():
+	print("You clicked the Map!")
+	$mapProgram/mapBkgd.show()
+## todo - add mini map to window
+
+func _on_power_supply_button_pressed():
+	print("You clicked the Power Supply!")
+	$powerSupplyProgram/powerSupplyBkgd.show()
+## todo - add power on and off buttons for different port numbers 
+
+func _on_passwords_button_pressed():
+	print("You clicked the Passwords!")
+	$passwordsProgram/passwordsBkgd.show()
+## todo - add password input options
+
+func _on_turning_button_pressed():
+	print("You clicked the Turning Button!")
+	$turningProgram/turningBkgd.show()
+## todo - add turn controls/buttons
+
+func _on_files_button_pressed():
+	print("You clicked the Files!")
+	$filesProgram/fileBkgd.show()
+## todo - add file repository (saved/encountered passwords/codes/etc)
+
+func _on_exit_button_pressed():
+	print("You closed the program!")
+	get_tree().call_group("Programs", "hide") 
