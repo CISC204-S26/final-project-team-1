cdc91a89456dea77fcd74cbd9fc980f8634caf08
MeatSeltzer <noahmason427@gmail.com>
2026-05-17 19:20:37 -0400

    pause static

--- final_project_all_together_now/Scripts/pause.gd
+++ final_project_all_together_now/Scripts/pause.gd
@@ -4,9 +4,9 @@ extends Control
 
 func _process(delta: float) -> void:
 	if Globals.paused == true:
-		$TempBG.show()
+		show()
 	else:
-		$TempBG.hide()
+		hide()
 	
 	if Input.is_action_just_pressed("pause"):
 		Globals.paused = !Globals.paused
