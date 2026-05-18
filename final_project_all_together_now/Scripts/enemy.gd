c7a9b7d115b329b0cd0110b1513b28e2b0e8f33c
Nincorp100 <c.gatewood.chiong@gmail.com>
2026-05-17 23:45:26 -0400

    Working on enemy sensor and pathing AI

--- final_project_all_together_now/Scripts/enemy.gd
+++ final_project_all_together_now/Scripts/enemy.gd
@@ -1,8 +1,9 @@
 extends CharacterBody3D
 
+var global = "res://Scripts/globals.gd"
 @export var move_speed = 2.0
 @onready var player : CharacterBody3D = $"../Player"
-
+@export var sensor_directions = [Vector3(0,0,-1), Vector3(1,0,-1), Vector3(1,0,0), Vector3(1,0,1), Vector3(0,0,1), Vector3(-1,0,1), Vector3(-1,0,0), Vector3(-1,0,-1)]
 
 func _physics_process(delta):
 		
@@ -10,6 +11,9 @@ func _physics_process(delta):
 	var dir = player.global_position - global_position
 	dir.y = 0.0
 	dir = dir.normalized()
+	if global.game_over == true:
+		print("game over!")
+	
 	
 	velocity = dir * move_speed
 	move_and_slide()
@@ -17,3 +21,15 @@ func _physics_process(delta):
 
 	
 	
+
+
+func _on_enemy_collision_area_area_entered(area: Area3D) -> void:
+	if area == $InteactableAreaInter:
+		global.game_over = true
+	else:
+		pass
+
+
+func _on_enemy_sensor_area_area_entered(area: Area3D) -> void:
+	if area == $InteactableAreaInter:
+		
