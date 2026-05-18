522cc9e9783fa81bf17cd68061896ff5a861031e
MeatSeltzer <noahmason427@gmail.com>
2026-05-17 20:16:47 -0400

    pause buttons and layout

--- final_project_all_together_now/Scripts/pause.gd
+++ final_project_all_together_now/Scripts/pause.gd
@@ -5,8 +5,10 @@ extends Control
 func _process(delta: float) -> void:
 	if Globals.paused == true:
 		show()
+		$CanvasLayer.show()
 	else:
 		hide()
+		$CanvasLayer.hide()
 	
 	if Input.is_action_just_pressed("pause"):
 		Globals.paused = !Globals.paused
