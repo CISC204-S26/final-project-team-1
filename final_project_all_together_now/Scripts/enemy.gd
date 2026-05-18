ae9991a9f32e515fa3fad58551c734695ea28eed
Nincorp100 <c.gatewood.chiong@gmail.com>
2026-05-17 23:55:45 -0400

    Gonna scream, fixed enemy.gd script error
    ahhhhhhhhhhhhhhh

--- final_project_all_together_now/Scripts/enemy.gd
+++ final_project_all_together_now/Scripts/enemy.gd
@@ -6,7 +6,13 @@ var global = "res://Scripts/globals.gd"
 @export var sensor_directions = [Vector3(0,0,-1), Vector3(1,0,-1), Vector3(1,0,0), Vector3(1,0,1), Vector3(0,0,1), Vector3(-1,0,1), Vector3(-1,0,0), Vector3(-1,0,-1)]
 
 func _physics_process(delta):
-		
+		pass
+	
+	
+
+	
+	
+func track_player(delta_):
 	## Directions to move to player's location, does not currently account for walls and such.
 	var dir = player.global_position - global_position
 	dir.y = 0.0
@@ -17,11 +23,6 @@ func _physics_process(delta):
 	
 	velocity = dir * move_speed
 	move_and_slide()
-	
-
-	
-	
-
 
 func _on_enemy_collision_area_area_entered(area: Area3D) -> void:
 	if area == $InteactableAreaInter:
@@ -32,4 +33,10 @@ func _on_enemy_collision_area_area_entered(area: Area3D) -> void:
 
 func _on_enemy_sensor_area_area_entered(area: Area3D) -> void:
 	if area == $InteactableAreaInter:
+		track_player
 		
+		
+
+
+func _on_enemy_sensor_area_area_exited(area: Area3D) -> void:
+	pass # Replace with function body.
